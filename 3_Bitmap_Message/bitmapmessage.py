51d3dc869bec502681bcf859249517d2b04c654a
Demegorash <felipe.solisgranados@gmail.com>
2024-09-02 22:05:16 -0600

    Book of Projects to learn to code in Python. 3_Bitmap_Message.  Project Completed

---
+++ 3_Bitmap_Message/bitmapmessage.py
@@ -0,0 +1,52 @@
+# Instructions on the instructions.txt file on this repo.  Please note, this is not my code, I only working and practicing by following the instructions at https://inventwithpython.com/bigbookpython/project3.html
+# All credits to them.
+
+import sys
+
+"""
+(!) Try changing this multiline string to any image you like:
+There are 68 periods along the top and bottom of this string:
+(You can also copy and paste this string from https://inventwithpython.com/bitmapworld.txt)
+"""
+bitmap = """
+....................................................................
+   **************   *  *** **  *      ******************************
+  ********************* ** ** *  * ****************************** *
+ **      *****************       ******************************
+          *************          **  * **** ** ************** *
+           *********            *******   **************** * *
+            ********           ***************************  *
+   *        * **** ***         *************** ******  ** *
+               ****  *         ***************   *** ***  *
+                 ******         *************    **   **  *
+                 ********        *************    *  ** ***
+                   ********         ********          * *** ****
+                   *********         ******  *        **** ** * **
+                   *********         ****** * *           *** *   *
+                     ******          ***** **             *****   *
+                     *****            **** *            ********
+                    *****             ****              *********
+                    ****              **                 *******   *
+                    ***                                       *    *
+                    **     *                    *
+....................................................................
+"""
+
+print("Bitmap Message, by Al Sweigart al@inventwithpython.com")
+print("Enter the message to display with the bitmap.")
+message = input("> ")
+if message == "":
+    sys.exit()
+    
+# Loop over each line in the bitmap:
+
+for line in bitmap.splitlines():
+    # Loop over each character in the line:
+    for i, bit in enumerate(line):
+        if bit == " ":
+            # Print and empty space since there is a space in the bitmap:
+            print(" ", end="")
+        else:
+            # Print a character from the message:
+            print(message[i % len(message)], end="")
+    print() # Print a new line.
